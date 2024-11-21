1553cc6572dffe49b70643d916ffaefde4a06421
Richard McSharry <rmcsharry@gmail.com>
2024-11-21 18:08:08 +0100

    chore: move to antfu eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,23 +1,6 @@
-// @ts-check
+// eslint.config.js
+import antfu from '@antfu/eslint-config'
 
-import eslint from '@eslint/js';
-import tseslint from 'typescript-eslint';
-import solid from "eslint-plugin-solid/configs/typescript";
-import * as tsParser from "@typescript-eslint/parser";
-import globals from 'globals';
-
-export default tseslint.config(
-  eslint.configs.recommended,
-  tseslint.configs.recommended,
-{
-    files: ["**/*.{ts,tsx}"],
-    ...solid,
-    languageOptions: {
-      parser: tsParser,
-      parserOptions: {
-        project: "tsconfig.json",
-      },
-      globals: globals.browser,
-    },
-  },
-);
+export default antfu({
+  solid: true,
+})
